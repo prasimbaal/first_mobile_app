55ab0d4bf9f57514d03334d9639ee55f00f0c1e2
prasimbaal <prasimbaal@gmail.com>
2024-05-03 13:00:54 +0100

    simple variable edit

--- app/src/main/java/com/mobilecoding/layoutapp/MainActivity.kt
+++ app/src/main/java/com/mobilecoding/layoutapp/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.mobilecoding.layoutapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mobilecoding.layoutapp.R.id.main
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linear1)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
